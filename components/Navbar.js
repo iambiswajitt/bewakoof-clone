03d29b1665c3cec69279d5cde1e0b8e48f8180fa
Ashishgithub2000 <142981978+Ashishgithub2000@users.noreply.github.com>
2023-08-31 10:48:29 +0530

    Add files via upload
    final upload of NAVBAR

--- components/Navbar.js
+++ components/Navbar.js
@@ -22,56 +22,56 @@ function Navbar() {
             <div id="nav_bar_search">
                 <div id="logo_menu">
 
-                    <li><a href="#"><img src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg" alt="logo" /></a></li>
+                    <li><a href="/"><img src="https://images.bewakoof.com/web/ic-desktop-bwkf-trademark-logo.svg" alt="logo" /></a></li>
                     <li><a href="https://www.bewakoof.com/men-clothing">MEN</a>
                         <div id="logo_sub_menu1">
                             <div id="sub_menu_item">
                                 <ul>
-                                    <li><a href="#"><h5>TopWear</h5></a></li>
-                                    <li><a href="#">Half Sleeve T-Shirt</a></li>
-                                    <li><a href="#">Full Slave T_Shirt</a></li>
-                                    <li><a href="#">Printed T-Shirt</a></li>
-                                    <li><a href="#"> Plain T-Shirt</a></li>
-                                    <li><a href="#">Vest</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Polo T-Shirt</a></li>
-                                    <li><a href="#">Shirts</a></li>
-                                    <li><a href="#">Kurtas</a></li>
+                                    <li><a href="/"><h5>TopWear</h5></a></li>
+                                    <li><a href="/">Half Sleeve T-Shirt</a></li>
+                                    <li><a href="/">Full Slave T_Shirt</a></li>
+                                    <li><a href="/">Printed T-Shirt</a></li>
+                                    <li><a href="/"> Plain T-Shirt</a></li>
+                                    <li><a href="/">Vest</a></li><li><a href="/p"></a></li>
+                                    <li><a href="/">Polo T-Shirt</a></li>
+                                    <li><a href="/">Shirts</a></li>
+                                    <li><a href="/">Kurtas</a></li>
 
                                 </ul>
                                 <ul>
-                                    <li><a href="#"><h5>Bottomwear</h5></a></li>
-                                    <li><a href="#">Pyjmas</a></li>
-                                    <li><a href="#">Boxers</a></li>
-                                    <li><a href="#">Sorts</a></li>
-                                    <li><a href="#"> Pnats</a></li>
-                                    <li><a href="#">Jeans</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Joggers</a></li>
-                                    <li><a href="#">Plus Size Store</a></li>
-                                    <li><a href="#">bRIEFS</a></li>
+                                    <li><a href="/"><h5>Bottomwear</h5></a></li>
+                                    <li><a href="/">Pyjmas</a></li>
+                                    <li><a href="/">Boxers</a></li>
+                                    <li><a href="/">Sorts</a></li>
+                                    <li><a href="/"> Pnats</a></li>
+                                    <li><a href="/">Jeans</a></li><li><a href="/h"></a></li>
+                                    <li><a href="/">Joggers</a></li>
+                                    <li><a href="/">Plus Size Store</a></li>
+                                    <li><a href="/">bRIEFS</a></li>
 
                                 </ul>
                                 <ul>
-                                    <li><a href="#"><h5>Ethnic Fusion Wear</h5></a></li>
-                                    <li><a href="#">Kurtans</a></li>
-                                    <li><a href="#">Neharu Jackets</a></li>
-                                    <li><a href="#"></a></li>
-                                    <li><a href="#"> <h5>Reginal</h5></a></li>
-                                    <li><a href="#">Marati T-Shirt</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Bengali T-Shirt</a></li>
-                                    <li><a href="#">Gujrati T-Shirt</a></li>
-                                    <li><a href="#">Tamil T-Shirt</a></li>
+                                    <li><a href="/"><h5>Ethnic Fusion Wear</h5></a></li>
+                                    <li><a href="/">Kurtans</a></li>
+                                    <li><a href="/">Neharu Jackets</a></li>
+                                    <li><a href="/"></a></li>
+                                    <li><a href="/"> <h5>Reginal</h5></a></li>
+                                    <li><a href="/">Marati T-Shirt</a></li><li><a href="/j"></a></li>
+                                    <li><a href="/">Bengali T-Shirt</a></li>
+                                    <li><a href="/">Gujrati T-Shirt</a></li>
+                                    <li><a href="/">Tamil T-Shirt</a></li>
 
                                 </ul>
                                 <hr />
                                 <ul class="top_category">
 
 
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt="" /><span>Amine Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/WinterWear-Bewakoof-Homepage-NavigationCircleIcon-1630653435.png" alt="" /><span>Winter Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/navi-collab-icon-1579514101.gif" alt="" /><span>Collabration</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/nav-icon-1617643368.png" alt="" /><span>Shop Unique Brands</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/new-arrival-icon-1625237098.png" alt="" /><span>Upcomming Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/bewakoof-men-logo-last-size-1592822253.png" alt="" /><span>Last Size Left</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt="/p" /><span>Amine Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/WinterWear-Bewakoof-Homepage-NavigationCircleIcon-1630653435.png" alt="/k" /><span>Winter Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/navi-collab-icon-1579514101.gif" alt="/m" /><span>Collabration</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/nav-icon-1617643368.png" alt="/t" /><span>Shop Unique Brands</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/new-arrival-icon-1625237098.png" alt="/m" /><span>Upcomming Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/bewakoof-men-logo-last-size-1592822253.png" alt="/l" /><span>Last Size Left</span></a></li>
 
 
                                 </ul>
@@ -83,48 +83,48 @@ function Navbar() {
                     <li><a href="https://www.bewakoof.com/women-clothing">WOMEN</a>
                         <div id="logo_sub_menu2">
                             <div id="sub_menu_item"><ul>
-                                <li><a href="#"><h5>TopWear</h5></a></li>
-                                <li><a href="#">Half Sleeve T-Shirt</a></li>
-                                <li><a href="#">Full Slave T_Shirt</a></li>
-                                <li><a href="#">Printed T-Shirt</a></li>
-                                <li><a href="#"> Kurties</a></li>
-                                <li><a href="#">Vest</a></li><li><a href="#"></a></li>
-                                <li><a href="#">Polo T-Shirt</a></li>
-                                <li><a href="#">Shirts</a></li>
-                                <li><a href="#">Kurtas</a></li>
+                                <li><a href="/"><h5>TopWear</h5></a></li>
+                                <li><a href="/">Half Sleeve T-Shirt</a></li>
+                                <li><a href="/">Full Slave T_Shirt</a></li>
+                                <li><a href="/">Printed T-Shirt</a></li>
+                                <li><a href="/"> Kurties</a></li>
+                                <li><a href="/">Vest</a></li><li><a href="/"></a></li>
+                                <li><a href="/">Polo T-Shirt</a></li>
+                                <li><a href="/">Shirts</a></li>
+                                <li><a href="/">Kurtas</a></li>
 
                             </ul>
                                 <ul>
-                                    <li><a href="#"><h5>Ethnic Fusion Wear</h5></a></li>
-                                    <li><a href="#">Kurtans</a></li>
-                                    <li><a href="#">Neharu Jackets</a></li>
-                                    <li><a href="#"></a></li>
-                                    <li><a href="#"> <h5>Reginal</h5></a></li>
-                                    <li><a href="#">Marati T-Shirt</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Bengali T-Shirt</a></li>
-                                    <li><a href="#">Gujrati T-Shirt</a></li>
-                                    <li><a href="#">Tamil T-Shirt</a></li>
+                                    <li><a href="/"><h5>Ethnic Fusion Wear</h5></a></li>
+                                    <li><a href="/">Kurtans</a></li>
+                                    <li><a href="/">Neharu Jackets</a></li>
+                                    <li><a href="/"></a></li>
+                                    <li><a href="/"> <h5>Reginal</h5></a></li>
+                                    <li><a href="/">Marati T-Shirt</a></li><li><a href="/"></a></li>
+                                    <li><a href="/">Bengali T-Shirt</a></li>
+                                    <li><a href="/">Gujrati T-Shirt</a></li>
+                                    <li><a href="/">Tamil T-Shirt</a></li>
 
                                 </ul>
                                 <ul>
-                                    <li><a href="#"><h5>Ethnic Fusion Wear</h5></a></li>
-                                    <li><a href="#">Kurtans</a></li>
-                                    <li><a href="#">Neharu Jackets</a></li>
-                                    <li><a href="#"></a></li>
-                                    <li><a href="#"> <h5>Reginal</h5></a></li>
-                                    <li><a href="#">Marati T-Shirt</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Bengali T-Shirt</a></li>
-                                    <li><a href="#">Gujrati T-Shirt</a></li>
-                                    <li><a href="#">Tamil T-Shirt</a></li>
+                                    <li><a href="/"><h5>Ethnic Fusion Wear</h5></a></li>
+                                    <li><a href="/">Kurtans</a></li>
+                                    <li><a href="/">Neharu Jackets</a></li>
+                                    <li><a href="/"></a></li>
+                                    <li><a href="/"> <h5>Reginal</h5></a></li>
+                                    <li><a href="/">Marati T-Shirt</a></li><li><a href="/"></a></li>
+                                    <li><a href="/">Bengali T-Shirt</a></li>
+                                    <li><a href="/">Gujrati T-Shirt</a></li>
+                                    <li><a href="/">Tamil T-Shirt</a></li>
 
                                 </ul>
                                 <ul class="top_category">
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt="" /><span>Amine Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/WinterWear-Bewakoof-Homepage-NavigationCircleIcon-1630653435.png" alt="" /><span>Winter Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/navi-collab-icon-1579514101.gif" alt="" /><span>Collebration</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/nav-icon-1617643368.png" alt="" /><span>Shop Unique Brands</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/new-arrival-icon-1625237098.png" alt="" /><span>Upcomming Collection</span></a></li>
-                                    <li><a href="#"><img src="https://images.bewakoof.com/nav_menu/bewakoof-men-logo-last-size-1592822253.png" alt="" /><span>Last Size Left</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/Anime-Bewakoof-Homepage-NavigationCircleIcon-final-1632292125.jpg" alt="" /><span>Amine Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/WinterWear-Bewakoof-Homepage-NavigationCircleIcon-1630653435.png" alt="" /><span>Winter Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/navi-collab-icon-1579514101.gif" alt="" /><span>Collebration</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/nav-icon-1617643368.png" alt="" /><span>Shop Unique Brands</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/new-arrival-icon-1625237098.png" alt="" /><span>Upcomming Collection</span></a></li>
+                                    <li><a href="/"><img src="https://images.bewakoof.com/nav_menu/bewakoof-men-logo-last-size-1592822253.png" alt="" /><span>Last Size Left</span></a></li>
 
 
                                 </ul>
@@ -135,37 +135,37 @@ function Navbar() {
                     <li><a href="https://www.bewakoof.com/mobile-covers-india">MOBILE COVER</a>
                         <div id="logo_sub_menu3">
                             <div id="sub_menu_item"><ul>
-                                <li><a href="#"><h2>POPULAR</h2></a></li>
-                                <li><a href="#"><h3>Xiaomi</h3></a></li>
-                                <li><a href="#">Realme 9 Power</a></li>
-                                <li><a href="#">Xiaomi Mi 10T</a></li>
-                                <li><a href="#">Xiaomi Redami K2</a></li>
-                                <li><a href="#"><h3>Sumsang Galaxy F62</h3></a></li><li><a href="#"></a></li>
-                                <li><a href="#">Sumsung Galaxy F41</a></li>
-                                <li><a href="#">Sumsumg Galaxy A30</a></li>
+                                <li><a href="/"><h2>POPULAR</h2></a></li>
+                                <li><a href="/"><h3>Xiaomi</h3></a></li>
+                                <li><a href="/">Realme 9 Power</a></li>
+                                <li><a href="/">Xiaomi Mi 10T</a></li>
+                                <li><a href="/">Xiaomi Redami K2</a></li>
+                                <li><a href="/"><h3>Sumsang Galaxy F62</h3></a></li><li><a href="/"></a></li>
+                                <li><a href="/">Sumsung Galaxy F41</a></li>
+                                <li><a href="/">Sumsumg Galaxy A30</a></li>
 
 
                             </ul>
                                 <ul>
-                                    <li><a href="#"><h3>Oneplus</h3></a></li>
-                                    <li><a href="#">Oneplus 9R</a></li>
-                                    <li><a href="#">Oneplus 8 pro</a></li>
-                                    <li><a href="#">Oneplus 8T</a></li>
-                                    <li><a href="#"> <h3>Realme</h3></a></li>
-                                    <li><a href="#">Realme 6</a></li><li><a href="#"></a></li>
-                                    <li><a href="#">Realme 5</a></li>
+                                    <li><a href="/"><h3>Oneplus</h3></a></li>
+                                    <li><a href="/">Oneplus 9R</a></li>
+                                    <li><a href="/">Oneplus 8 pro</a></li>
+                                    <li><a href="/">Oneplus 8T</a></li>
+                                    <li><a href="/"> <h3>Realme</h3></a></li>
+                                    <li><a href="/">Realme 6</a></li><li><a href="/"></a></li>
+                                    <li><a href="/">Realme 5</a></li>
 
 
                                 </ul>
                                 <ul>
-                                    <li><a href="#"><h3>Apple</h3></a></li>
-                                    <li><a href="#">iphone 13</a></li>
-                                    <li><a href="#">iphone 12</a></li>
-                                    <li><a href="#">iphone 12 pro</a></li>
-                                    <li><a href="#"><h3>Vivo</h3></a></li>
-                                    <li><a href="#">vovo vii Pro</a></li><li><a href="#"></a></li>
-                                    <li><a href="#"> Vivo X60</a></li>
-                                    <li><a href="#">Vivo Y91</a></li>
+                                    <li><a href="/"><h3>Apple</h3></a></li>
+                                    <li><a href="/">iphone 13</a></li>
+                                    <li><a href="/">iphone 12</a></li>
+                                    <li><a href="/">iphone 12 pro</a></li>
+                                    <li><a href="/"><h3>Vivo</h3></a></li>
+                                    <li><a href="/">vovo vii Pro</a></li><li><a href="/"></a></li>
+                                    <li><a href="/"> Vivo X60</a></li>
+                                    <li><a href="/">Vivo Y91</a></li>
 
 
                                 </ul>
@@ -175,7 +175,7 @@ function Navbar() {
                                 </ul>
 
                                 <ul>
-                                    <li><a href="#"><span></span><img src="https://images.bewakoof.com/nav_menu/bewakoof-online-fashion-COTM-mobile-cover-navigation-box-desktop-1612275399.jpg" alt="" /></a></li>
+                                    <li><a href="/"><span></span><img src="https://images.bewakoof.com/nav_menu/bewakoof-online-fashion-COTM-mobile-cover-navigation-box-desktop-1612275399.jpg" alt="" /></a></li>
 
 
                                 </ul>
